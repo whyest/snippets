743cafcf3fe039944645f08962927b509be213af
Денис Галкин <whyest@gmail.com>
2024-03-09 13:37:13 +0300

    modify snippet page

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -2,58 +2,58 @@ import { redirect } from 'next/navigation'
 import { db } from '@/db'
 
 const SnippetCreatePage = () => {
-  async function createSnippet(formData: FormData) {
-    // This needs to be a server action!
-    'use server'
+	async function createSnippet(formData: FormData) {
+		// This needs to be a server action!
+		'use server'
 
-    // Check the user's inputs and make sure they're valid
-    const title = formData.get('title') as string
-    const code = formData.get('code') as string
+		// Check the user's inputs and make sure they're valid
+		const title = formData.get('title') as string
+		const code = formData.get('code') as string
 
-    // Create a new record in the database
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code,
-      },
-    })
-    console.log(snippet)
+		// Create a new record in the database
+		const snippet = await db.snippet.create({
+			data: {
+				title,
+				code,
+			},
+		})
+		console.log(snippet)
 
-    // Redirect the user back to the root route
-    redirect('/')
-  }
+		// Redirect the user back to the root route
+		redirect('/')
+	}
 
-  return (
-    <form action={createSnippet}>
-      <h3 className='font-bold m-3'>Create a Snippet</h3>
-      <div className='flex flex-col gap-4'>
-        <div className='flex gap-4'>
-          <label className='w-12' htmlFor='title'>
-            Title
-          </label>
-          <input
-            name='title'
-            className='border rounded p-2 w-full'
-            id='title'
-          />
-        </div>
+	return (
+		<form action={createSnippet}>
+			<h3 className='font-bold m-3'>Create a Snippet</h3>
+			<div className='flex flex-col gap-4'>
+				<div className='flex gap-4'>
+					<label className='w-12' htmlFor='title'>
+						Title
+					</label>
+					<input
+						name='title'
+						className='border rounded p-2 w-full'
+						id='title'
+					/>
+				</div>
 
-        <div className='flex gap-4'>
-          <label className='w-12' htmlFor='code'>
-            Code
-          </label>
-          <textarea
-            name='code'
-            className='border rounded p-2 w-full'
-            id='code'
-          />
-        </div>
+				<div className='flex gap-4'>
+					<label className='w-12' htmlFor='code'>
+						Code
+					</label>
+					<textarea
+						name='code'
+						className='border rounded p-2 w-full'
+						id='code'
+					/>
+				</div>
 
-        <button type='submit' className='rounded p-2 bg-blue-200'>
-          Create
-        </button>
-      </div>
-    </form>
-  )
+				<button type='submit' className='rounded p-2 bg-blue-200'>
+					Create
+				</button>
+			</div>
+		</form>
+	)
 }
 export default SnippetCreatePage
