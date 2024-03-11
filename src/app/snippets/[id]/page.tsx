b582d4334dfcfaa92cb4aea117466b982b632a22
whyest <whyest@gmail.com>
2024-03-11 17:45:55 +0300

    add dynamic paths and notFound page

---
+++ src/app/snippets/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { notFound } from 'next/navigation'
+import { db } from '@/db'
+
+interface SnippetShowPageProps {
+  params: {
+    id: string
+  }
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  const snippet = await db.snippet.findFirst({
+    where: { id: parseInt(props.params.id) },
+  })
+
+  if (!snippet) {
+    return notFound()
+  }
+
+  console.log(props)
+  return <h1>{snippet.title}</h1>
+}
