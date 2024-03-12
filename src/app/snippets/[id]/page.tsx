975be5b33676071b9a67057e09369d7226cf5ddf
whyest <whyest@gmail.com>
2024-03-12 17:58:34 +0300

    add style to show page

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,22 +1,35 @@
-import { notFound } from 'next/navigation'
-import { db } from '@/db'
+import { notFound } from "next/navigation";
+import { db } from "@/db";
 
 interface SnippetShowPageProps {
   params: {
-    id: string
-  }
+    id: string;
+  };
 }
 
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
-  await new Promise((r) => setTimeout(r, 500))
+  await new Promise((r) => setTimeout(r, 500));
   const snippet = await db.snippet.findFirst({
     where: { id: parseInt(props.params.id) },
-  })
+  });
 
   if (!snippet) {
-    return notFound()
+    return notFound();
   }
 
-  console.log(props)
-  return <h1>{snippet.title}</h1>
+  console.log(props);
+  return (
+    <div>
+      <div className="flex justify-between items-center my-4">
+        <h1 className="text-xl font-bold capitalize">{snippet.title}</h1>
+        <div className="flex gap-3">
+          <button className="p-2 border rounded">Edit</button>
+          <button className="p-2 border rounded">Delete</button>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
 }
