95edd49f130ef8745caeae8a088f9ce03f4ba5ed
whyest <whyest@gmail.com>
2024-03-12 17:46:53 +0300

    add loaging page, links to paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
-import { db } from '@/db'
+import Link from "next/link";
+import { db } from "@/db";
+
 export default async function Home() {
-  const snippets = await db.snippet.findMany()
+  const snippets = await db.snippet.findMany();
   const renderedSnippets = snippets.map((snippet) => {
-    return <div key={snippet.id}>{snippet.title}</div>
-  })
-  return <div>{renderedSnippets}</div>
+    return (
+      <Link
+        key={snippet.id}
+        href={`/snippets/${snippet.id}`}
+        className="flex justify-between p-2 items-center border rounded"
+      >
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
+    );
+  });
+  return (
+    <div>
+      <div className="flex justify-between items-center my-2">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link href={`/snippets/new`} className="border rounded p-2 px-3">
+          New
+        </Link>
+      </div>
+      <div className="flex gap-2 flex-col">{renderedSnippets}</div>
+    </div>
+  );
 }
--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -8,6 +8,7 @@ interface SnippetShowPageProps {
 }
 
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  await new Promise((r) => setTimeout(r, 500))
   const snippet = await db.snippet.findFirst({
     where: { id: parseInt(props.params.id) },
   })
