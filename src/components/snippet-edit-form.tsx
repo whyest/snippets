877044030847e3a22e4cf54c84914f7e80a08e40
whyest <whyest@gmail.com>
2024-03-13 17:45:10 +0300

    add snippet edit form

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -1,9 +1,26 @@
 "use client";
 import type { Snippet } from "@prisma/client";
+import Editor from "@monaco-editor/react";
+import { useState } from "react";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
-  return <div>Client component has the title {snippet.title}</div>;
+  const [code, setCode] = useState(snippet.code);
+  const handleEditorChange = (value: string = "") => {
+    setCode(value);
+  };
+  return (
+    <div>
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        language="javascript"
+        defaultValue={snippet.code}
+        options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
+      />
+    </div>
+  );
 }
